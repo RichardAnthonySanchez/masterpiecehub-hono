ed13af0f250d6f3a57508083f839ce914b431445
richard <theirsaviorhd@gmail.com>
2025-11-11 15:27:01 -0500

    Serving and Rendering Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./src/static/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        display: ["Almendra", "Georgia", "serif"],
+        subheading: ["Comfortaa", "Arial", "sans-serif"],
+        body: ["CrimsonPro", "Times New Roman", "serif"],
+      },
       fontSize: {
         base: ["2.5rem", { lineHeight: "4rem" }],
       },
